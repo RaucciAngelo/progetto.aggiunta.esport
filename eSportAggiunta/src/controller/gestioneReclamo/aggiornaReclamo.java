0cb5fabee76fc90c90b157c35fbc0c7c7391240b
raulp <raulp@LAPTOP-4JL9DJHC>
2020-03-21 20:07:38 +0100

    Modifica classi

--- eSportAggiunta/src/controller/gestioneReclamo/aggiornaReclamo.java
+++ eSportAggiunta/src/controller/gestioneReclamo/aggiornaReclamo.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class aggiornaSegnalazione
+ * Servlet implementation class aggiornaReclamo
  */
-@WebServlet("/aggiornaSegnalazione")
-public class aggiornaSegnalazione extends HttpServlet {
+@WebServlet("/aggiornaReclamo")
+public class aggiornaReclamo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public aggiornaSegnalazione() {
+    public aggiornaReclamo() {
         super();
         // TODO Auto-generated constructor stub
     }
--- eSportAggiunta/src/controller/gestioneReclamo/sottomissioneReclamo.java
+++ eSportAggiunta/src/controller/gestioneReclamo/sottomissioneReclamo.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class sottomissioneSegnalazione
+ * Servlet implementation class sottomissioneReclamo
  */
-@WebServlet("/sottomissioneSegnalazione")
-public class sottomissioneSegnalazione extends HttpServlet {
+@WebServlet("/sottomissioneReclamo")
+public class sottomissioneReclamo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public sottomissioneSegnalazione() {
+    public sottomissioneReclamo() {
         super();
         // TODO Auto-generated constructor stub
     }
